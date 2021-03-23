19a1d012d2196988a47ad825751db20ef91865b4
Arthur Schamroth <schamrotharthur@gmail.com>
2021-03-23 19:50:40 +0100

    inscription/connexion liaison base de données

--- projet_symfony/src/Controller/SecurityController.php
+++ projet_symfony/src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController {
 
@@ -18,5 +19,40 @@ class SecurityController extends AbstractController {
     public function mdp_oublie()
     {
         return $this->render('Security/mdp_oublie.html.twig');
+    }
+
+    
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(AuthenticationUtils $authenticationUtils): Response
+    {
+        return new Response("Vous êtes connecté !");
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+             return $this->redirectToRoute('home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
