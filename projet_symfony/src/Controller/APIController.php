c0ab0f31e887a84d28e66bbf1ddc1a12060567aa
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2021-03-29 16:51:19 +0200

    Controller Api début

--- projet_symfony/src/Controller/APIController.php
+++ projet_symfony/src/Controller/APIController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('api', name: 'api_')]
-
-class APIController extends AbstractController
+class ApiController extends AbstractController
 {
-    #[Route('/a/p/i', name: 'a_p_i')]
-    public function index(): Response
+    /**
+     * @Route("/api/user", name="api_user_index", methods={"GET"})  
+     */
+    
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
+        dd($users);
+
         return $this->render('api/index.html.twig', [
-            'controller_name' => 'APIController',
+            'controller_name' => 'ApiController',
         ]);
     }
 }
