c3d95fcde6ce059e40fbf838f845866754a00bb0
Arthur Schamroth <schamrotharthur@gmail.com>
2021-04-02 17:03:54 +0200

    Ajout de la section commentaire (il faut corriger quelques points de boostrap)

---
+++ projet_symfony/src/Controller/commentaireController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class commentaireController extends AbstractController{
+    #[Route('/commentaires', name: 'commentaires')]
+    public function comment(Request $request, EntityManagerInterface $manager): Response
+    {   
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime());
+ 
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('commentaires');
+        }
+        
+        $commentaires = $this->getDoctrine()->getRepository(Comment::class)->findAll();
+
+        return $this->render("commentaires/commentaires.html.twig", [
+            'commentForm'=>$form->createView(),
+            'commentaires'=>$commentaires
+        ]);
+    }
+}
