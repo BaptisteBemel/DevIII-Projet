05e9d6a172af9aec91714ebeabab3a658694173b
Hotmailnico <63592532+Hotmailnico@users.noreply.github.com>
2021-04-07 16:22:56 +0200

    Changes to be committed: 	new file:   src/Controller/MessagerieController.php 	new file:   templates/messagerie/messagerie.html.twig 	new file:   templates/messagerie/messagerie.js

---
+++ projet_symfony/src/Controller/MessagerieController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MessagerieController extends AbstractController{
+    
+    /**
+     * @Route("/messagerie", name="messagerie", methods={"GET"})  
+     */
+    //#[Route('/messagerie', name: 'messagerie')]
+    public function messagerie(): Response
+    {
+        return $this->render('./messagerie/messagerie.html.twig', [
+            'controller_name' => 'messagerieController',
+        ]);
+    }
+}
+?>
