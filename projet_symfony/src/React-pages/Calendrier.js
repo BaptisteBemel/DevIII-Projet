fa69514c12515ec89df7264f1dfae14b5f6ef6b0
bygum <bygum2010@gmail.com>
2021-04-15 16:30:25 +0200

    US4- Essai de liaison avec React - pas fonctionnel

---
+++ projet_symfony/src/React-pages/Calendrier.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Calendrier(){
+    return(
+        <div>
+            <h1>Toto</h1>
+        </div>
+    );
+}
+
+export default Calendrier;
---
+++ projet_symfony/templates/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';    
+
+function Calendrier(){
+    return <Link to="/calendrier">Calendrier</Link>;
+}
+
+export default Calendrier;
