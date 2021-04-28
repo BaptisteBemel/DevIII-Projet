83ae824a7cbacc8dc72b25884ab1b3056de43f8e
Hotmailnico <63592532+Hotmailnico@users.noreply.github.com>
2021-04-28 12:30:42 +0200

    up

--- projet_symfony/src/Controller/ApiController.php
+++ projet_symfony/src/Controller/ApiController.php
@@ -26,10 +26,16 @@ class ApiController extends AbstractController
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
     /**
      * @Route("/api/main.css", name="api_css")  
+=======
+
+    /**
+     * @Route("/api/main.css", name="api_css", methods={"GET"})  
+>>>>>>> parent of 2d67b37 ( Changes to be committed:)
 =======
 
     /**
@@ -50,7 +56,10 @@ class ApiController extends AbstractController
     }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 060c043016cd766857fa17122c9b19a9a4673282
+=======
+>>>>>>> parent of 2d67b37 ( Changes to be committed:)
 =======
 >>>>>>> parent of 2d67b37 ( Changes to be committed:)
 =======
