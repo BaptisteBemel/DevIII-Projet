5b5172d502cc0b33d47b530e4e78a45d927e2649
bygum <bygum2010@gmail.com>
2021-05-09 17:25:58 +0200

    inutile-pour pouvoir pull

--- projet_symfony/assets/calendrier/component/PostApiDispo.js
+++ projet_symfony/assets/calendrier/component/PostApiDispo.js
@@ -4,10 +4,10 @@ import axios from 'axios'
 class PostApiDispo extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             date_rdv: '',
-             statut: 'libre'
+            date_rdv: '',
+            statut: 'libre'
         }
     }
 
@@ -19,22 +19,34 @@ class PostApiDispo extends Component {
         e.preventDefault()
         console.log(this.state)
         axios.post('/api/dispo/post', this.state)
-        .then(response => {
-            console.log(response);
-            let data = new Array();
-            data.push(this.sate)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .then(response => {
+                console.log(response);
+                let data = new Array();
+                data.push(this.sate)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
-    
+
     render() {
         const { date_rdv } = this.state
         return (
             <div>
                 <input type="datetime-local" name="date_rdv" value={date_rdv} onChange={this.changeHandler} />
                 <button onClick={this.submitHandler}>Ajouter des disponibilites</button>
+                <div id="calendrierAffichage">
+                <table class="table" id="tableAffichage">
+                    <tr>
+                        <td scope="col">Date</td>
+                        <td>Heure</td>
+                        <td>Cours</td>
+                        <td>Status</td>
+                    </tr>
+                    <div>
+                    </div>
+                </table>
+                </div>
             </div>
         )
     }
