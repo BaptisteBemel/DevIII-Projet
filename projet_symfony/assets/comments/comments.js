d628d6d57ca9387857c54d6113c2c650bdbd02bf
Arthur Schamroth <schamrotharthur@gmail.com>
2021-05-18 19:28:58 +0200

    réparation des bétises

--- projet_symfony/assets/comments/comments.js
+++
@@ -1,8 +0,0 @@
-class CommentsElement extends HTMLElement {
-
-    connectedCallback(){
-        this.innerHTML = "Bonjour tout le monde"
-    }
-}
-
-// Uncaught TypeError: Failed to execute 'define' on 'CustomElementRegistry': parameter 2 is not of type 'Function'.
