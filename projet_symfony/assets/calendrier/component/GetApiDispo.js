d9a244e340b7177dfa2a30010d46573648e208ff
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2021-05-19 16:07:55 +0200

    essai de réparation

---
+++ projet_symfony/assets/calendrier/component/GetApiDispo.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { render } from 'react-dom'
+
+function affichage(données){
+    document.getElementById('titre').innerHTML = '<th>Plages horaire disponibles</th>';
+    document.getElementById('tbAffichage').innerHTML = '';
+    données.forEach(x => {
+        document.getElementById('tbAffichage').innerHTML += '<tr>'+ x.dateRdv + '</tr>'
+    })
+}
+
+class GetApiDispo extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            date_rdv: '',
+            statut: 'libre'
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+        axios.get('/api/dispo/get', this.state)
+            .then(response => {
+                affichage(response.data)
+            })
+    } //console.log(response.data) affichage(response.data)
+
+    
+    render() {
+        return (
+            <div>
+                <div>
+                    <button onClick={this.submitHandler}>Afficher des disponibilites</button>
+                </div>
+                <div>
+                    <table>
+                        <thead>
+                            <tr id='titre'>
+                            </tr>
+                        </thead>
+                        <tbody id='tbAffichage'>
+                            <tr>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+export default GetApiDispo;
+
+/*
+<div>
+                    <table>
+                        <thead>
+                            <tr id='titre'>
+                                <th>Plages horaire disponibles</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr id="trAffichage">
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>*/
