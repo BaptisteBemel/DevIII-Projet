5c2f9e366121e38426d3643e33d17d6e584b4709
Arthur Schamroth <schamrotharthur@gmail.com>
2021-05-28 14:43:06 +0200

    Dernier testing

---
+++ tests/DB/UserRepositoryTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Tests\DB; 
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserRepositoryTest extends KernelTestCase
+{
+    public function testCount(){
+        self::bootKernel();
+        $users = self::$container->get(UserRepository::class)->count([]);
+        $this->assertEquals(3, $users);
+    }
+}
